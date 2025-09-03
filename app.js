7bb60a27260b48e14754db1f016d946cee99963d
a09288092-ship-it <a09288092@gmail.com>
2025-09-03 17:41:43 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+// условия (if else,swich-case),операторы
+
+// boolean
+
+// let num='5'
+// let num2=5
+
+// console.log(num==num2)
+// console
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
