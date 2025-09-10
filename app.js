0a892c0edf043d7545f9c9b5e6c084f77630b7ca
a09288092-ship-it <a09288092@gmail.com>
2025-09-11 00:10:16 +0600

    first commit

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
-// условия (if else,swich-case),операторы
 
-// boolean
-
-// let num='5'
-// let num2=5
-
-// console.log(num==num2)
-// console
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
 
 
+one.onclick = function () {
+  document.body.style.backgroundColor = 'yellow';
+  one.style.marginLeft="200px"
+};
+  one.style.marginLeft="200px"
 
-
-
-
-
-
-
+two.onclick = function () {
+  document.body.style.backgroundColor = 'blue';
+};
 
 
 
